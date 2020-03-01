760d26941325c9960e7d6e5447eff2a095646f03
shailyn <sortizjim@gmail.com>
2020-03-01 19:46:45 -0400

    adding aws lambda support and the test document

--- reader.py
+++ reader.py
@@ -1,4 +1,10 @@
+# Built-in
 import re
+import json
+# Third party
+import boto3
+
+s3 = boto3.resource('s3')
 
 
 def read_document(doc: str):
@@ -16,14 +22,29 @@ def extract_values(text: str):
     if (_from := re.search(r"cierre\sdel\s(.*?),", text)):
         data.append(_from.group())
 
-    if (until := re.search(r"hasta\sel\s.*?(\d.+?)\.",text)):
+    if (until := re.search(r"hasta\sel\s.*?(\d.+?)\.", text)):
         data.append(until.group())
 
     return data
 
 
+def lambda_handler(event, context):
+    s3_data = event['Records'][0]['s3']
+    bucket = s3_data['bucket']
+    _object = s3_data['object']
+
+    obj = s3.Object(bucket['name'], _object['key'])
+    data = obj.get()['Body'].read().decode('utf-8')
+    data = extract_values(data)
+
+    return json.dumps({
+        'statusCode': 200,
+        'body': data
+    })
+
+
 def main():
-    print(extract_values(read_document('tasaus_mc.txt')))
+    return extract_values(read_document('tasaus_mc.txt'))
 
 
 if __name__ == "__main__":
