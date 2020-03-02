b9e9b882b27143a00af51dcd04c69e4d75a6b1f6
shailyn <sortizjim@gmail.com>
2020-03-01 20:08:10 -0400

    converting response into a dictionary, saving it to dynamodb

--- reader.py
+++ reader.py
@@ -5,6 +5,14 @@ import json
 import boto3
 
 s3 = boto3.resource('s3')
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('dollar')
+
+
+def save_record(data: dict):
+    table.put_item(
+        Item=data
+    )
 
 
 def read_document(doc: str):
@@ -20,12 +28,12 @@ def extract_values(text: str):
     )
 
     if (_from := re.search(r"cierre\sdel\s(.*?),", text)):
-        data.append(_from.group())
+        data.append(_from.group(1))
 
     if (until := re.search(r"hasta\sel\s.*?(\d.+?)\.", text)):
-        data.append(until.group())
-
-    return data
+        data.append(until.group(1))
+    
+    return dict(zip(['buy', 'sell', 'from', 'until'], data))
 
 
 def lambda_handler(event, context):
@@ -36,6 +44,7 @@ def lambda_handler(event, context):
     obj = s3.Object(bucket['name'], _object['key'])
     data = obj.get()['Body'].read().decode('utf-8')
     data = extract_values(data)
+    save_record(data)
 
     return json.dumps({
         'statusCode': 200,
@@ -48,4 +57,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    print(main())
